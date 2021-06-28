b04deb6bda62dddb3351876405c9a8c9c0b7244c
pakjkwan <pakjkwan@gmail.com>
2021-06-28 14:19:40 +0900

    react code update

--- frontend/src/templates/Home.jsx
+++ frontend/src/templates/Home.jsx
@@ -1,28 +1,38 @@
 import React, {useEffect} from 'react'
 import axios from 'axios';
-
+import { Button } from '@material-ui/core';
 
 const Home = ({children}) => { 
+    /*
     useEffect(()=>{
+        
+    },[])
+    */
+    
+    const handleClick = e => {
+        e.preventDefault()
         axios({
             method: "get",
-            url: "http://127.0.0.1:8000/hello",
+            url: "http://127.0.0.1:8000/connection",
             responseType: "json"
-        }).then(function (response) {
-            alert(response.data.greeting)
+        }).then(function (res) {
+            localStorage.setItem('connection',res.data.connection)
         });
-    },[])
-    
-    
+    }
+
     return (<>
     <table className="tab_lay">
         <tr><td><h1>홈</h1></td></tr>
-        <tr><td><button >서버 연결 테스트</button></td></tr>
+        <tr><td><Button color="primary" onClick={handleClick}>서버 연결 테스트</Button></td></tr>
+        <tr><td>{ localStorage.getItem("connection") === 'SUCCESS' ?
+        '연결상태 입니다.'
+         : 
+        '연결상태가 아닙니다.' 
+         }</td></tr>
     </table>
     {children}
 
 </>)}
-
 
 export default Home
 
