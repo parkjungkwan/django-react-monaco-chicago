a5fa81ab158ec84b2d543b4a4059f114b845875d
pakjkwan <pakjkwan@gmail.com>
2021-06-28 16:23:32 +0900

    16:23 update

--- frontend/src/templates/Blog.jsx
+++ frontend/src/templates/Blog.jsx
@@ -1,2 +1,11 @@
 import React from 'react'
+import { UserMenu as Menu } from '../common'
+import './table.style.css'
 
+const Blog = ({children}) => (<>
+    <h1>Blog</h1>
+    <Menu/>
+    {children}
+</>)
+
+export default Blog
