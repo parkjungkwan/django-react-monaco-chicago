27cef85e419f929607c9ac2a3c4971155e39168e
pakjkwan <pakjkwan@gmail.com>
2021-07-01 14:34:20 +0900

    14

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -20,8 +20,8 @@ export const itemRegister = body => axios.post(`${SERVER}item/register`,{headers
 export const itemRetrieve = body => axios.post(`${SERVER}item/retrieve`,{headers, body})
 /* Member */
 export const memberDetail = body => axios.post(`${SERVER}member/detail`,{headers, body})
-export const memberDelete = body => axios.post(`${SERVER}member/delete`,{headers, body})
-export const memberList = body => axios.post(`${SERVER}member/list`,{headers, body})
+export const memberDelete = body => axios.delete(`${SERVER}member/delete`,{headers, body})
+export const memberList = () => axios.get(`${SERVER}member/list`)
 export const MemberModify = body => axios.post(`${SERVER}member/modify`,{headers, body})
 export const memberRegister = body => axios.post(`${SERVER}member/register`,{headers, body})
 export const memberRetrieve = body => axios.post(`${SERVER}member/retrieve`,{headers, body})
--- frontend/src/member/components/MemberList.js
+++ frontend/src/member/components/MemberList.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
@@ -8,6 +8,7 @@ import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 import Pagination from '@material-ui/lab/Pagination';
+import { memberList } from 'api'
 
 const useStyles = makeStyles({
   table: {
@@ -25,46 +26,53 @@ const usePageStyles = makeStyles((theme) => ({
 
 
 
-const createData = (name, calories, fat, carbs, protein) => {
-  return { name, calories, fat, carbs, protein };
-}
-
-const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
-
 const MemberList = () => {
+  
+  const [members, setMembers] = useState([])
+
   const classes = useStyles();
   const pageClasses = usePageStyles();
+
+  useEffect(() => {
+    memberList()
+    .then(res => {
+        console.log(res.data)
+        setMembers(res.data)
+    })
+    .catch(err => {
+        console.log(err.data)
+    })
+  }, [])
 
   return (<>
     <TableContainer component={Paper}>
       <Table className={classes.table} aria-label="simple table">
         <TableHead>
           <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+            <TableCell>회원 ID</TableCell>
+            <TableCell align="right">비밀번호</TableCell>
+            <TableCell align="right">회원 이름</TableCell>
+            <TableCell align="right">이메일</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows.map((row) => (
-            <TableRow key={row.name}>
-              <TableCell component="th" scope="row">
-                {row.name}
-              </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
-            </TableRow>
-          ))}
+          { members.length != 0
+           ? members.map((member) => {
+               <TableRow key={member.username}>
+               <TableCell component="th" scope="row">
+               {member.password}
+               </TableCell>
+               <TableCell align="right">{member.name}</TableCell>
+               <TableCell align="right">{member.email}</TableCell>
+           </TableRow>
+          })
+          :  <TableRow>
+          <TableCell component="th" scope="row" colSpan="4">
+             <h1>등록된 데이터가 없습니다</h1>
+          </TableCell>
+        
+      </TableRow>
+          }
         </TableBody>
       </Table>
     </TableContainer>
@@ -78,3 +86,14 @@ const MemberList = () => {
 }
 
 export default MemberList
+
+/*
+ <TableRow key={row.name}>
+    <TableCell component="th" scope="row">
+    {row.name}
+    </TableCell>
+    <TableCell align="right">{row.calories}</TableCell>
+    <TableCell align="right">{row.fat}</TableCell>
+    <TableCell align="right">{row.carbs}</TableCell>
+</TableRow>
+*/
