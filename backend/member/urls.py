57a14d82a3ad7d1a79abefc8d99513553b0468ae
pakjkwan <pakjkwan@gmail.com>
2021-07-01 10:27:37 +0900

    10

--- backend/member/urls.py
+++ backend/member/urls.py
@@ -3,6 +3,6 @@ from .views import Members as members
 from .views import Member as member
 from django.urls import path, include
 urlpatterns = [
-    url('register', members.as_view()),
-    path('<int:pk>/', member.as_view()),
+    url('/register', members.as_view()),
+    path('/<int:pk>/', member.as_view()),
 ]
--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -3,5 +3,11 @@ import axios from 'axios'
 const SERVER = 'http://127.0.0.1:8000/'
 const headers = {'Content-Type': 'application/json'}
 
+
+export const memberDetail = body => axios.post(`${SERVER}member/detail`,{headers, body})
+export const memberDelete = body => axios.post(`${SERVER}member/delete`,{headers, body})
+export const memberList = body => axios.post(`${SERVER}member/list`,{headers, body})
+export const MemberModify = body => axios.post(`${SERVER}member/modify`,{headers, body})
 export const memberRegister = body => axios.post(`${SERVER}member/register`,{headers, body})
+export const memberRetrieve = body => axios.post(`${SERVER}member/retrieve`,{headers, body})
 export const memberLogin = body => axios.post(`${SERVER}member/login`,{headers, body})
