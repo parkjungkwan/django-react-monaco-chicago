6bde25a91e725220f3e906d89bf21a88a1b64b0e
parkjungkwan <pakjkwan@gmail.com>
2021-07-05 10:37:11 +0900

    local reset

--- frontend/src/common/components/Menu.js
+++ frontend/src/common/components/Menu.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+/**
 export const MemberMenu = () => ( <nav>
        
             
@@ -23,6 +24,20 @@ export const MemberMenu = () => ( <nav>
             
        
 </nav>
+
+        )
+ */
+        export const MemberMenu = () => ( <nav>
+                    <li><Link to='/member-login'>회원로그인</Link></li>
+                    <li><Link to='/member-register'>회원등록</Link></li>
+                    <li><Link to='/member-list'>회원목록</Link></li>
+                    <li><Link to='/member-retreive'>회원이름조회</Link></li>
+                    <li><Link to='/member-detail'>회원상세</Link></li>
+                    <li><Link to='/member-modify'>회원수정</Link></li>
+                    <li><Link to='/member-delete'>회원탈퇴</Link></li>
+                    <li><Link to='/member-logout'>로그아웃</Link></li>
+       
+        </nav>
 
         )
 export const ItemMenu = () => (<nav>
